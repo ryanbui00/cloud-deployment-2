21013b084c9fe4a0cbec3b31f9e5eece75a37bd1
ryanbui00 <43189020+ryanbui00@users.noreply.github.com>
2018-09-13 09:14:27 -0700

    Update profile.py

--- profile.py
+++ profile.py
@@ -8,17 +8,19 @@ Wait for the profile instance to start, then click on the node in the topology a
 #Import the Portal object.
 import geni.portal as portal
 import geni.rspec.pg as pg
+link = request.LAN("lan")
 # Create a portal context.
 pc = portal.Context()
 
 # Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
-link = request.LAN("lan")
 for i in range(0,4):
     node = request.XenVM(str("Node-") + str(i+1))
     iface = node.addInterface("if" + str(i+1))
     node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+# Specify the component id and the IPv4 address
     iface.component_id = "eth1"
+# Set a specific IP address on each node
     iface.addAddress(pg.IPv4Address("192.168.1."+str(i+1),"255.255.255.0"))
     
     link.addInterface(iface)
